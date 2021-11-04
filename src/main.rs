2762c2137b49801804bde032d2fe98b94bc82362
SebastianJL <johannes.lade@yahoo.com>
2021-11-04 10:32:29 +0100

    Added burning ship fractal. (High memory usage.)

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use image::GrayImage;
 use ndarray::{Array1, Array2};
-use num_complex::Complex64;
+use num_complex::Complex32;
 
-fn mandelbrot(z: Complex64, c: Complex64) -> Complex64 {
+fn mandelbrot(z: Complex32, c: Complex32) -> Complex32 {
     return z * z + c;
 }
 
-fn burning_ship(z: Complex64, c: Complex64) -> Complex64 {
-    let i = Complex64::new(0., 1.);
+fn burning_ship(z: Complex32, c: Complex32) -> Complex32 {
+    let i = Complex32::new(0., 1.);
     return (z.re.abs() + i * z.im.abs()).powu(2) + c;
 }
 
-fn fatou_grid<F>(grid: &Array2<Complex64>, f: F, div_radius: f64, max_iter: u32) -> Array2<u32>
+fn fatou_grid<F>(grid: &Array2<Complex32>, f: F, div_radius: f32, max_iter: u32) -> Array2<u32>
 where
-    F: Fn(Complex64, Complex64) -> Complex64,
+    F: Fn(Complex32, Complex32) -> Complex32,
 {
     let mut iter_grid = Array2::<u32>::zeros(grid.raw_dim());
     for i in 0..grid.nrows() {
         for j in 0..grid.ncols() {
-            let mut z = Complex64::new(0., 0.);
+            let mut z = Complex32::new(0., 0.);
             for iter in 0..max_iter {
                 let c = grid[(i, j)];
                 z = f(z, c);
@@ -38,12 +38,12 @@ where
 /// Creates a 2d grid of complex numbers with `xrange` and `yrange` coordinates.
 /// Functions similar to numpy.meshgrid but instead of computing two separate 2d arrays
 /// for x and y coordinates it puts them in a 2d grid of complex numbers.
-fn complex_grid(xrange: Array1<f64>, yrange: Array1<f64>) -> Array2<Complex64> {
-    let mut grid = Array2::<Complex64>::zeros((xrange.len(), yrange.len()));
+fn complex_grid(xrange: Array1<f32>, yrange: Array1<f32>) -> Array2<Complex32> {
+    let mut grid = Array2::<Complex32>::zeros((xrange.len(), yrange.len()));
 
     for (i, x) in xrange.into_iter().enumerate() {
         for (j, y) in yrange.clone().into_iter().enumerate() {
-            grid[(i, j)] = Complex64::new(x, y);
+            grid[(i, j)] = Complex32::new(x, y);
         }
     }
 
@@ -54,10 +54,15 @@ fn array_to_grayscale(arr: Array2<u32>) -> GrayImage {
     assert!(arr.is_standard_layout());
     assert!(arr.len() > 0);
 
-    let arr_u8 = Array2::<u8>::zeros(arr.raw_dim());
+    let mut arr_u8 = Array2::<u8>::zeros(arr.raw_dim());
 
     let max: u32 = *arr.iter().max().expect("empty array");
-    arr_u8 = (arr * 255 / max) as Array2<u8>;
+
+    for i in 0..arr.nrows() {
+        for j in 0..arr.ncols() {
+            arr_u8[[i, j]] = (arr[[i, j]] * 255 / max) as u8;
+        }
+    }
 
     let (height, width) = arr_u8.dim();
     let raw = arr_u8.into_raw_vec();
@@ -68,15 +73,15 @@ fn array_to_grayscale(arr: Array2<u32>) -> GrayImage {
 
 fn main() {
     // Draw mandelbrot
-    if 1 == 0 {
+    if 1 == 1 {
         let (xmin, xmax) = (-2.5, 1.);
         let (ymin, ymax) = (-1., 1.);
         let n_grid_x = 20_000.;
         let n_grid_y = n_grid_x * (ymax - ymin) / (xmax - xmin);
         let n_grid_x = n_grid_x as usize;
         let n_grid_y = n_grid_y as usize;
-        let x = Array1::<f64>::linspace(xmin, xmax, n_grid_x);
-        let y = Array1::<f64>::linspace(ymin, ymax, n_grid_y);
+        let x = Array1::<f32>::linspace(xmin, xmax, n_grid_x);
+        let y = Array1::<f32>::linspace(ymin, ymax, n_grid_y);
         let grid = complex_grid(x, y);
 
         let max_iter = 255;
@@ -89,18 +94,18 @@ fn main() {
     }
 
     // Draw burning ship
-    if 1 == 1 {
+    if 1 == 0 {
         let (xmin, xmax) = (-4., 4.);
         let (ymin, ymax) = (-4., 4.);
-        let n_grid_x = 50_000.;
+        let n_grid_x = 20_000.;
         let n_grid_y = n_grid_x * (ymax - ymin) / (xmax - xmin);
         let n_grid_x = n_grid_x as usize;
         let n_grid_y = n_grid_y as usize;
-        let x = Array1::<f64>::linspace(xmin, xmax, n_grid_x);
-        let y = Array1::<f64>::linspace(ymin, ymax, n_grid_y);
+        let x = Array1::<f32>::linspace(xmin, xmax, n_grid_x);
+        let y = Array1::<f32>::linspace(ymin, ymax, n_grid_y);
         let grid = complex_grid(x, y);
 
-        let max_iter = 255;
+        let max_iter = 500;
         let div_radius = 4.;
         let iter_grid = fatou_grid(&grid, burning_ship, div_radius, max_iter);
         let iter_grid = array_to_grayscale(iter_grid);
